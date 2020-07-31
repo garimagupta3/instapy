b4a0bef3c63adf3503aa582d1cf6404b06a748c5
garimagupta3 <46891596+garimagupta3@users.noreply.github.com>
2020-07-31 14:44:30 +0530

    Update basic_follow-unfollow_activity.py

--- quickstart_templates/basic_follow-unfollow_activity.py
+++ quickstart_templates/basic_follow-unfollow_activity.py
@@ -45,7 +45,10 @@ with smart_run(session):
     except Exception:
       print(traceback.format_exc())
     
-    
+    try:
+      print('Hey webhook')
+    except Exception:
+      print(traceback.format_exc())
     """ Activity flow """
     # general settings
     session.set_relationship_bounds(enabled=True,
